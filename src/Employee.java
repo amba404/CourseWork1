7dbc87ada4b41185c77db81d55e880cc529cc6ad
amba404 <amba.avg@gmail.com>
2024-09-09 16:33:48 +0900

    add method getAllSalary

---
+++ src/Employee.java
@@ -0,0 +1,80 @@
+import java.util.Objects;
+
+import static java.lang.Math.round;
+
+public class Employee {
+    private static int nextId = 0;
+    final private int id;
+    final private String fullName;
+    private int department;
+    private float salary;
+
+    public Employee(String fullName, int department, float salary) {
+        if (fullName == null) throw new IllegalArgumentException("Не указаны ФИО");
+        this.fullName = fullName.trim();
+        this.setDepartment(department);
+        this.setSalary(salary);
+        this.id = getNextId();
+    }
+
+    private static int getNextId() {
+        nextId++;
+        return nextId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        int minDepNumber = 1;
+        int maxDepNumber = 5;
+        if (department >= minDepNumber && department <= maxDepNumber) {
+            this.department = department;
+        } else {
+            throw new IllegalArgumentException("Недопустимый номер отдела");
+        }
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        if (salary < 0) {
+            throw new IllegalArgumentException("Значение зарплаты меньше нуля");
+        }
+        this.salary = (float) (Math.round(salary * 100) / 100);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && Objects.equals(fullName, employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         Employee[] emploees = new Employee[10];
-        emploees[0] = new Employee("Иванов Иван Иванович", 1, 15_000);
-        emploees[1] = new Employee("Петров Петр Петрович", 1, 16_000);
-        emploees[2] = new Employee("Сидоров Сидор Сидорович", 1, 17_000);
+        emploees[0] = new Employee("Иванов Иван Иванович", 1, 15_000f);
+        emploees[1] = new Employee("Петров Петр Петрович", 2, 16_000f);
+        emploees[2] = new Employee("Сидоров Сидор Сидорович", 1, 17_000f);
 
         printAllEmployees(emploees);
+        System.out.printf("Всего затрат на зарплату: %.2f", getAllSalary(emploees));
     }
 
-    public static void printAllEmployees(Employee[] arr){
-        for (final Employee empl : arr){
-            if(empl != null){
+    public static void printAllEmployees(Employee[] arr) {
+        for (final Employee empl : arr) {
+            if (empl != null) {
                 System.out.println(empl);
             }
         }
+    }
+
+    public static float getAllSalary(Employee[] arr) {
+        float sum = 0;
+        for (final Employee empl : arr) {
+            if (empl != null) {
+                sum += empl.getSalary();
+            }
+        }
+        return sum;
     }
 
 }
