ac3bb873b3c9ff97d5da8df52e92d9e497eac051
amba404 <amba.avg@gmail.com>
2024-09-09 16:42:10 +0900

    add method getEmployeeMaxSalary

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Employee[] emploees = new Employee[10];
-        emploees[0] = new Employee("Иванов Иван Иванович", 1, 15_000f);
-        emploees[1] = new Employee("Петров Петр Петрович", 2, 16_000f);
-        emploees[2] = new Employee("Сидоров Сидор Сидорович", 1, 17_000f);
+        emploees[0] = new Employee("Иванов Иван Иванович", 1, 150_000f);
+        emploees[1] = new Employee("Петров Петр Петрович", 2, 160_000f);
+        emploees[2] = new Employee("Сидоров Сидор Сидорович", 1, 170_000f);
 
         printAllEmployees(emploees);
         System.out.printf("Всего затрат на зарплату: %.2f\n", getAllSalary(emploees));
         System.out.printf("Сотрудник с минимальной зарплатой: %s\n", getEmployeeMinSalary(emploees));
+        System.out.printf("Сотрудник с максимальной зарплатой: %s\n", getEmployeeMaxSalary(emploees));
     }
 
     public static void printAllEmployees(Employee[] arr) {
@@ -40,4 +41,15 @@ public class Main {
         return ret;
     }
 
+    public static Employee getEmployeeMaxSalary(Employee[] arr) {
+        float maxSalary = 0;
+        Employee ret = null;
+        for (final Employee empl : arr) {
+            if (empl != null && maxSalary < empl.getSalary()) {
+                maxSalary = empl.getSalary();
+                ret = empl;
+            }
+        }
+        return ret;
+    }
 }
