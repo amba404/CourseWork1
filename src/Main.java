84f97bc98cd1554a5694a5b8d580fa094624e2c5
amba404 <amba.avg@gmail.com>
2024-09-09 16:29:46 +0900

    Create class Employee add method PrintAllEmployees

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Employee[] emploees = new Employee[10];
+        emploees[0] = new Employee("Иванов Иван Иванович", 1, 15_000);
+        emploees[1] = new Employee("Петров Петр Петрович", 1, 16_000);
+        emploees[2] = new Employee("Сидоров Сидор Сидорович", 1, 17_000);
 
+        printAllEmployees(emploees);
     }
+
+    public static void printAllEmployees(Employee[] arr){
+        for (final Employee empl : arr){
+            if(empl != null){
+                System.out.println(empl);
+            }
+        }
+    }
+
 }
